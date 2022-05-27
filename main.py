6cf68f4b9af0cc4077be33aa77f274f951df4910
MustafaBaig51 <baigmustafa47@gmail.com>
2022-05-27 20:24:05 +0500

    created minimax algorithem

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame
-from checkers.constants import SQUARE_SIZE, WIDTH, HEIGHT
-from checkers.board import Board
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
 from checkers.game import Game
 
 FPS = 60
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Checkers - Minimax - Mustafa Baig - 17908')
+pygame.display.set_caption('Checkers')
 
 def get_row_col_from_mouse(pos):
     x, y = pos
@@ -17,15 +17,25 @@ def main():
     run = True
     clock = pygame.time.Clock()
     game = Game(WIN)
+
     while run:
         clock.tick(FPS)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: 
+            if event.type == pygame.QUIT:
                 run = False
+            
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
-        game.update( )
+                game.select(row, col)
+
+        game.update()
+    
     pygame.quit()
 
 main()
